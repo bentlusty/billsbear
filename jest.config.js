8b829500dc50aa802e4b099ad4ed3c421bdbc908
bentlusty <tlustybentlustyben@gmail.com>
2021-10-29 09:47:13 +0300

    Added basic infra for testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  transformIgnorePatterns: ['^.+\\.js$'],
+  testEnvironment: 'node',
+};
